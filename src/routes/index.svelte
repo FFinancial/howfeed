14644034ac175c4e1a14254e05f3cd1c2a6122d4
James Shiffer <basedvioletfag@waifu.club>
2020-06-04 10:05:53 -0700

    [wip] new styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,59 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+    h1, figure, p {
+        text-align: center;
+        margin: 0 auto;
+    }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+    h1 {
+        font-size: 2.8em;
+        text-transform: uppercase;
+        font-weight: 700;
+        margin: 0 0 0.5em 0;
+    }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    figure {
+        margin: 0 0 1em 0;
+    }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+    img {
+        width: 100%;
+        max-width: 400px;
+        margin: 0 0 1em 0;
+    }
 
-	p {
-		margin: 1em auto;
-	}
+    p {
+        margin: 1em auto;
+    }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    @media (min-width: 480px) {
+        h1 {
+            font-size: 4em;
+        }
+    }
+
+    div.background {
+        background: url('/cityscape.jpg') no-repeat center;
+        background-size: cover;
+        position: fixed;
+        height: 16rem;
+        width: 100vw;
+        z-index: 0;
+        filter: blur(5px);
+    }
+    div.content {
+        margin-top: 5rem;
+        z-index: 1;
+    }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+    <title>HOWFEED.BIZ</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="background"></div>
+<div class="content">
+    <h1 class="uppercase">Welcome</h1>
+    <figure>
+        <figcaption>Have fun with Sapper!</figcaption>
+    </figure>
+</div>
