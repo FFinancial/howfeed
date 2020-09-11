bc2a56383d92018eb79e93124d942810a98a9784
scoliono <2191476+scoliono@users.noreply.github.com>
2020-09-10 20:24:57 -0700

    fixed rss feed for ie7

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ import fs from 'fs';
 import cors from 'cors';
 import helmet from 'helmet';
 import useragent from 'useragent';
+import RSS from 'rss';
 import path from 'path';
 import crypto from 'crypto';
 import Article from './models/article.js';
@@ -391,7 +392,44 @@ mainRouter
                 }));
             }
         }
-    );
+    )
+	.get('/rss.xml', async function (req, res) {
+		let year = new Date().getFullYear();
+		let feed = new RSS({
+			title: 'HowFeed.biz Articles',
+			feed_url: 'http://howfeed.biz/rss.xml',
+			site_url: 'http://howfeed.biz/',
+			image_url: 'http://howfeed.biz/logo.png',
+			language: 'en',
+			webMaster: 'webmaster@howfeed.biz',
+			copyright: `${year} FemboyFinancial Holdings Co., Ltd. (USA LLC)`
+		});
+		let articles = await Article.find().populate({
+			path: 'author',
+			select: 'realname avatar'
+		}).populate({
+			path: 'category'
+		});
+
+		for (let article of articles) {
+			feed.item({
+				title: article.title,
+				description: article.html,
+				url: `http://howfeed.biz/a/${article.slug}`,
+				categories: [ article.category.name ],
+				author: article.author.realname,
+				date: article.created_at,
+				enclosure: {
+					url: `http://howfeed.biz/a/${article.image}`
+				}
+			});
+		}
+
+		res.writeHead(200, {
+			'Content-Type': 'application/rss+xml'
+		});
+		res.end(feed.xml());
+	});
 
 app.use(helmet())
     .use(cors())
